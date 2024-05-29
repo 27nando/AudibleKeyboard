b6bca3c49fbafa320e30575ebb4e3d2e98a8e2e3
Nandini Gupta <nagupta@tcd.ie>
2024-05-29 21:08:36 +0100

    creating a button class

--- sketch.js
+++ sketch.js
@@ -8,3 +8,29 @@ function setup() {
 function draw() {
   background(204, 255, 229);
 }
+
+class Button {
+  constructor(x, y, w, h, color, accent, song) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
+    this.accent = accent;
+    this.song = song;
+  }
+}
+
+function show() {
+  noStroke();
+
+  fill(this.color); 
+  rect(this.x, this.y, this.w, this.h);
+
+  fill(this.accent); 
+  ellipse(this.x, this.y, this.w, this.h);
+
+  fill(this.color); 
+  arc(this.x, this.y, this.w, this.h, TWO_PI, PI);
+}
+
