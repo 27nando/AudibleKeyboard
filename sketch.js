80d666913c5758da6ca530d70cd4b9bd7fd9f7d1
Nandini Gupta <nagupta@tcd.ie>
2024-05-30 18:16:44 +0100

    added sounds for middle row

--- sketch.js
+++ sketch.js
@@ -5,57 +5,58 @@ let buttons_top = [];
 let buttons_middle = [];
 let buttons_bottom = [];
 
-let arcade;
-let bonus1;
-let bonus2;
-let fairy;
-let lock;
-let magic;
-let notify;
-let retro;
-let score;
+let A;
+let S;
+let D;
+let F;
+let G;
+let H;
+let J;
+let K;
+let L;
 
 function setup() {
   canvas = createCanvas(width, height);
 
-  arcade = loadSound('arcade.wav');
-  bonus1 = loadSound('bonus1.wav');
-  bonus2 = loadSound('bonus2.wav');
-  fairy = loadSound('fairy.wav');
-  lock = loadSound('lock.wav');
-  magic = loadSound('magic.wav');
-  notify = loadSound('notify.wav');
-  retro = loadSound('retro.wav');
-  score = loadSound('score.wav');
+  A = loadSound('A.mp3');
+  S = loadSound('S.mp3');
+  D = loadSound('D.mp3');
+  F = loadSound('F.mp3');
+  G = loadSound('G.mp3');
+  H = loadSound('H.mp3');
+  J = loadSound('J.mp3');
+  K = loadSound('K.mp3');
+  L = loadSound('L.mp3');
 
-  let q = new Button(width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let w = new Button(2*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), bonus1);
-  let e = new Button(3*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), bonus2);
-  let r = new Button(4*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let t = new Button(5*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let y = new Button(6*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let u = new Button(7*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let i = new Button(8*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let o = new Button(9*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
-  let p = new Button(10*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), arcade);
 
-  let a = new Button(width/9, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
-  let s = new Button(20*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), lock);
-  let d = new Button(29*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), magic);
-  let f = new Button(38*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
-  let g = new Button(47*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
-  let h = new Button(56*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
-  let j = new Button(65*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
-  let k = new Button(74*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
-  let l = new Button(83*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), fairy);
+  let q = new Button(width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let w = new Button(2*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let e = new Button(3*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let r = new Button(4*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let t = new Button(5*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let y = new Button(6*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let u = new Button(7*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let i = new Button(8*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let o = new Button(9*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
+  let p = new Button(10*width/11, height/2, 100, 40, color(255, 128, 0), color(250, 149, 0), A);
 
-  let z = new Button(width/6, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), notify);
-  let x = new Button(17*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), retro);
-  let c = new Button(23*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), score);
-  let v = new Button(29*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), notify);
-  let b = new Button(35*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), notify);
-  let n = new Button(41*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), notify);
-  let m = new Button(47*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), notify);
+  let a = new Button(width/9, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), A);
+  let s = new Button(20*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), S);
+  let d = new Button(29*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), D);
+  let f = new Button(38*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), F);
+  let g = new Button(47*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), G);
+  let h = new Button(56*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), H);
+  let j = new Button(65*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), J);
+  let k = new Button(74*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), K);
+  let l = new Button(83*width/99, 2*height/3, 100, 40, color(205, 125, 208), color(232, 142, 237), L);
+
+  let z = new Button(width/6, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
+  let x = new Button(17*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
+  let c = new Button(23*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
+  let v = new Button(29*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
+  let b = new Button(35*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
+  let n = new Button(41*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
+  let m = new Button(47*width/66, 5*height/6, 100, 40, color(210, 91, 32), color(235, 100, 36), A);
 
   buttons_top.push(q);
   buttons_top.push(w);
@@ -101,13 +102,13 @@ function mousePressed() {
 
 function mouseReleased() {
   for (let i = 0; i < buttons_top.length; i++) {
-    buttons_top[i].y = height/3.5;
+    buttons_top[i].y = height/2;
   }
   for (let i = 0; i < buttons_middle.length; i++) {
-    buttons_middle[i].y = height/2;
+    buttons_middle[i].y = 2*height/3;
   }
   for (let i = 0; i < buttons_bottom.length; i++) {
-    buttons_bottom[i].y = 2*height/2.8;
+    buttons_bottom[i].y = 5*height/6;
   }
 }
 
@@ -153,7 +154,7 @@ class Button {
     let d = dist(px, py, this.x, this.y);
 
     if (d < this.w/2) {
-      this.y = this.y - 10;
+      this.y = this.y + 10;
       this.song.play();
     }
   }
